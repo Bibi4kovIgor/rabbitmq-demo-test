0013457691fa1f0bde5f64b91afc293e47cfc9dc
ihor <work.bibi4kov@gmail.com>
2024-11-13 13:47:42 +0200

    Demo of Work Queues:  Refactored

--- src/main/java/workqueues/Worker.java
+++ src/main/java/workqueues/Worker.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 public class Worker {
 
     private static final String TASK_QUEUE_NAME = "task_queue";
@@ -21,7 +23,7 @@ public class Worker {
         channel.basicQos(1);
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
 
             System.out.println(" [x] Received '" + message + "'");
             try {
